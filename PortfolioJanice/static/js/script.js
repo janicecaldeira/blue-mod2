184f9aa6625c4ce49159da820726779aa8cf1a27
janicecaldeira <janicecaldeira@gmail.com>
2021-07-25 17:27:41 -0300

    Atualizando

--- PortfolioJanice/static/js/script.js
+++ PortfolioJanice/static/js/script.js
@@ -1,14 +1,13 @@
 let nome = document.querySelector('name')
 let email = document.querySelector('email')
 let mensagem = document.querySelector('message')
-let btnEnviar = document.querySelector('btn btn-big btn-primary')
 let form = document.querySelector('form')
 
 nome.addEventListener('keyup', () => {
     if (nome.value.length < 3) {
-       nome.style.inputColor = 'red'
+       nome.style.color = 'red'
     } else if(nome.value == '' || nome.value == null) {
-        nome.style.fontColor = 'white'
+        nome.style.color = 'white'
     } else {
        nome.style.color = 'green'
     }
@@ -16,33 +15,21 @@ nome.addEventListener('keyup', () => {
 
 email.addEventListener('keyup', () => {
     if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
-        email.style.borderColor = '#E03F3D'
-        emailOk = false
+        email.style.color = 'red'
     } else if(email.value == '' || email.value == null) {
-        email.style.borderColor = '#E03F3D'
-        emailOk = false
+        email.style.color = 'white'
     } else {
-        email.style.borderColor = 'green'
-        emailOk = true
+        email.style.color = 'green'
     }
 })
 
 mensagem.addEventListener('keyup', () => {
     if (mensagem.value.length < 10 || mensagem.value.length > 150) {
-       mensagem.style.borderColor = '#E03F3D'
-       msgOk = false
+       mensagem.style.borderColor = 'red'
     } else if(mensagem.value == '' || mensagem.value == null) {
-        mensagem.style.borderColor = '#E03F3D'
-        msgOk = false
+        mensagem.style.borderColor = 'white'
     } else {
        mensagem.style.borderColor = 'green'
-       msgOk = true
-    }
-
-    if (nomeOk && emailOk && msgOk) {
-        btnEnviar.disabled = false
-    } else {
-        btnEnviar.disabled = true
     }
  })
 
